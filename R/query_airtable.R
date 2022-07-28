0e52f9d1afe43c497f7a7124262c44086ce860cb
lathanliou <lltk2015@mymail.pomona.edu>
2022-07-27 22:31:40 -0400

    try to add automatic auth

--- R/query_airtable.R
+++ R/query_airtable.R
@@ -10,6 +10,7 @@
 #' query_airtable()
 
 query_airtable <- function(sheet_url = "https://docs.google.com/spreadsheets/d/1dCOcNwOYZDQXeyAHnc5ypDyaKmwm0OOKWq5nFAWR924/edit#gid=417127965") {
+  options(gargle_oauth_email = "lathanliu21@gmail.com")
   read_sheet(sheet_url) %>%
     clean_names()
 }
